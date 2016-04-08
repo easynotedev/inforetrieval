f80b753e5f04c18f4ba0d56b285c51a475ef2193
easynotedev <tuffasspc@4802ED>
2016-04-07 23:44:56 -0500

    comments

---
+++ Assign4/pagerank.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+=pod
+
+=head1 TITLE
+Name          : pagerank.pl
+Author        : Antonin Karlo M. Tilaon
+Course Info   : COSC 4315.001 - Assignment IV
+Date          : 4/7/2016
+=cut
+
+=pod
+
+=head1 DESCRIPTION
+       : Compute and output the link probability matrix formed by the documents
+       : inputs the teleport probability alpha
+       : along with the number of desired iterations
+       : Display the page rank of each document using the input teleport probability
+       : Documents should be displayed in sorted (decreasing) order
+       : Based on the computed popularity.
+=cut
